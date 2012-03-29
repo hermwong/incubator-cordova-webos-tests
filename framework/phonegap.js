34a2f0ab9edee0aadc232c5378063ce00a50b95e
hermwong <herm.wong@gmail.com>
2012-03-29 13:39:21 -0700

    update test harness to run against cordova 1.6.0

--- framework/phonegap.js
+++ framework/phonegap.js
@@ -1,6 +1,6 @@
-document.write('<script type="text/javascript" charset="utf-8" src="../../phonegap-1.3.0.js"></script>');
-document.write('<script type="text/javascript" charset="utf-8" src="../phonegap-1.3.0.js"></script>');
-document.write('<script type="text/javascript" charset="utf-8" src="phonegap-1.3.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="../../cordova-1.6.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="../cordova-1.6.0.js"></script>');
+document.write('<script type="text/javascript" charset="utf-8" src="cordova-1.6.0.js"></script>');
 
 function backHome() {
 	if (device.platform.toLowerCase() == 'android') {
